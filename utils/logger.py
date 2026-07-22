cae16629417df3fcba32a3b25014afa64586bb91
ops523 <ops@adwallz.com>
2026-07-22 14:43:20 +0530

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,16 @@
+import logging
+import os
+
+os.makedirs("logs", exist_ok=True)
+
+logging.basicConfig(
+
+    filename="logs/aims.log",
+
+    level=logging.INFO,
+
+    format="%(asctime)s %(levelname)s %(message)s"
+
+)
+
+logger = logging.getLogger("AIMS")
