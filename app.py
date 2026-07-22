f2235b8f793ebdb50c50f53a216b80144adbfca8
ops523 <ops@adwallz.com>
2026-07-22 14:40:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,15 @@
 import streamlit as st
 
-from database import init_database
-
-from auth import login
-
-from modules import dashboard
-from modules import inventory
-from modules import production
-from modules import wastage
-from modules import reports
-from modules import settings
-
 st.set_page_config(
-    page_title="Media Roll Tracker",
+
+    page_title="AIMS ERP",
+
     layout="wide"
+
 )
 
-init_database()
+st.title("AIMS ERP")
 
-if not login():
-    st.stop()
+st.success("Foundation Created Successfully")
 
-st.sidebar.title("ADWALLZ")
-
-page = st.sidebar.radio(
-    "Navigation",
-    [
-        "Dashboard",
-        "Inventory",
-        "Production",
-        "Wastage",
-        "Reports",
-        "Settings"
-    ]
-)
-
-if page == "Dashboard":
-    dashboard.show()
-
-elif page == "Inventory":
-    inventory.show()
-
-elif page == "Production":
-    production.show()
-
-elif page == "Wastage":
-    wastage.show()
-
-elif page == "Reports":
-    reports.show()
-
-elif page == "Settings":
-    settings.show()
+st.write("Sprint 1 - Part 1")
